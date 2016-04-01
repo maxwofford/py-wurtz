118a0ec6b0248160314b57a2903f51e43f99cd48
Max Wofford <max@maxwofford.com>
2016-04-01 19:56:14 -0400

    Add numpy fourier transform

--- py-wurtz.py
+++ py-wurtz.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import pyaudio
+import numpy as np
 
 CHUNK = 1024
 WIDTH = 2
@@ -8,24 +9,29 @@ CHANNELS = 2
 RATE = 44200
 RECORD_SECONDS = 5
 
+def createStream():
+    p = pyaudio.PyAudio()
+    stream = p.open(
+        format=p.get_format_from_width(WIDTH),
+        channels=CHANNELS,
+        rate=RATE,
+        input=True,
+        output=True,
+        frames_per_buffer=CHUNK
+    )
+    return stream, p
 
-p = pyaudio.PyAudio()
-
-stream = p.open(format=p.get_format_from_width(WIDTH),
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                output=True,
-                frames_per_buffer=CHUNK
-)
-
-print("* recording")
-
-while True:
-    data = stream.read(CHUNK)
-    stream.write(data, CHUNK)
-
-print("* done")
-stream.stop_stream()
-stream.close()
-p.terminate()
+stream, p = createStream()
+print("* listening")
+try:
+    while True:
+        data = stream.read(CHUNK)
+        signal = np.array([np.fromstring(data, 'Float32')], dtype=float)
+        fourier = np.fft.fft(signal)
+        freq = np.fft.fftfreq(signal.size, d = 1 / RATE)
+        stream.write(data, CHUNK)
+except (KeyboardInterrupt, SystemExit):
+    print("* done")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
